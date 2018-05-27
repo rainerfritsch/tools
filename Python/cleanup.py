537e9b45ae56a9e4381ed56dfc1ddd22eeceebdf
Rainer Fritsch <mail@rainerfritsch.de>
2018-05-27 11:04:28 +0200

    platform independence - step 1

--- Python/cleanup.py
+++ Python/cleanup.py
@@ -1,19 +1,24 @@
 import os
+from pathlib import Path
+
 
 # config
-baseDirectory = "/home/rainer/"
+baseDirectory = str(Path.home())+"/"
 downloadDirectory = baseDirectory + "Downloads/"
-documentDirectory = baseDirectory + "Downloads/Documents/"
-pictureDirectory = baseDirectory + "Downloads/Pictures/"
-installationDirectory= baseDirectory + "Downloads/Installationfiles/"
-musicDirectory = baseDirectory + "Downloads/Music/"
-videoDirectory = baseDirectory + "Downloads/Videos/"
+documentDirectory = downloadDirectory + "_Documents/"
+pictureDirectory = downloadDirectory + "_Pictures/"
+installationDirectory= downloadDirectory + "_Installationfiles/"
+musicDirectory = downloadDirectory + "_Music/"
+videoDirectory = downloadDirectory + "_Videos/"
+archiveDirectory = downloadDirectory + "_Archive/"
 
 # files ending with this ending fall into that category
 txtfiles=".txt,.doc,.pdf,.xlsx"
-musicfiles=".mp3"
-installationfiles=".zip,.dmg,.tar.gz,.tar.bz2,.iso,.exe"
+musicfiles=".mp3,.wav"
+installationfiles=".dmg,.iso,.exe"
 videofiles=".mp4"
+picturefiles=".jpg,.jpeg,.png"
+archivefiles=".zip,.tar,.gz,.bz2"
 
 # decide if file ends with given fileending
 def kindOf(filename, endings):
@@ -25,21 +30,42 @@ def kindOf(filename, endings):
     #if filename ends with certain filending return true
     return (isKindOf>0)
 
-# create needed directories
-#def init()
+# create folders if nessesary
+def assure_path_exists(path):
+        dir = os.path.dirname(path)
+        if not os.path.exists(dir):
+                os.makedirs(dir)
 
+def check():
+    assure_path_exists(documentDirectory)
+    assure_path_exists(pictureDirectory)
+    assure_path_exists(installationDirectory)
+    assure_path_exists(musicDirectory)
+    assure_path_exists(videoDirectory)
+    assure_path_exists(archiveDirectory)
 
+# let´s go
+def main():
+    check()
+    for filename in os.listdir(downloadDirectory):
+        if (kindOf(filename,txtfiles)):
+            print ("*Textfile found * "+filename)
+            os.rename(downloadDirectory+filename,documentDirectory+filename)
+        if (kindOf(filename,musicfiles)):
+            print ("*Music found * "+filename)
+            os.rename(downloadDirectory+filename,musicDirectory+filename)
+        if (kindOf(filename,installationfiles)):
+            print ("*Installation file found * "+filename)
+            os.rename(downloadDirectory+filename,installationDirectory+filename)
+        if (kindOf(filename,videofiles)):
+            print ("*Video file found * "+filename)
+            os.rename(downloadDirectory+filename,videoDirectory+filename)
+        if (kindOf(filename,picturefiles)):
+            print ("*Picture file found * "+filename)
+            os.rename(downloadDirectory+filename,pictureDirectory+filename)
+        if (kindOf(filename,archivefiles)):
+            print ("*Picture file found * "+filename)
+            os.rename(downloadDirectory+filename,archiveDirectory+filename)
 
-for filename in os.listdir(downloadDirectory):
-    if (kindOf(filename,txtfiles)):
-        print ("*Textfile found * "+filename)
-        os.rename(downloadDirectory+filename,documentDirectory+filename)
-    if (kindOf(filename,musicfiles)):
-        print ("*Music found * "+filename)
-        os.rename(downloadDirectory+filename,musicDirectory+filename)
-    if (kindOf(filename,installationfiles)):
-        print ("*Installation file found * "+filename)
-        os.rename(downloadDirectory+filename,installationDirectory+filename)
-    if (kindOf(filename,videofiles)):
-        print ("*Video file found * "+filename)
-        os.rename(downloadDirectory+filename,videoDirectory+filename)
+if __name__ == "__main__":
+    main()
